11ef5916a444617688c26d203f7606057c76005d
Yuliya Papina <panterajuliapapina@yandex.ru>
2023-02-01 22:30:54 +0300

    доделаны js и readme

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
 let content = document.querySelector('.content')
 let popup = document.querySelector('.popup');
-let profileEditButton = content.querySelector('.profile__button_edit');
-let popupButtonClose = popup.querySelector('.popup__close');
+let profileButtonEdit = content.querySelector('.profile__button_edit');
+let popupClose = popup.querySelector('.popup__close');
 let popupForm = popup.querySelector('.popup__container');
-let nameInput = popupForm.querySelector('.popup__input_name');
-let jobInput = popupForm.querySelector('.popup__input_job');
+let nameInput = popupForm.querySelector('.popup__input_type_name');
+let jobInput = popupForm.querySelector('.popup__input_type_job');
 
-function popupStateChange() {
+function popupChange() {
     popup.classList.toggle('popup_opened'); 
     popupForm.reset(); // 
   }
   
-  profileEditButton.addEventListener('click', popupStateChange); 
-  popupButtonClose.addEventListener('click', popupStateChange); 
+  profileButtonEdit.addEventListener('click', popupChange); 
+  popupClose.addEventListener('click', popupChange); 
   
   function handleFormSubmit(evt) {
     evt.preventDefault(); 
@@ -28,7 +28,7 @@ function popupStateChange() {
     nameInput.setAttribute('value', name);
     jobInput.setAttribute('value', job);
   
-    popupStateChange();
+    popupChange();
   }
   
   popupForm.addEventListener('submit', handleFormSubmit);
