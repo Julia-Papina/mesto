8960c37e7154d5ef0d2f562aec316a990a78ba24
Yuliya Papina <panterajuliapapina@yandex.ru>
2023-02-04 00:18:47 +0300

    исправление index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,34 @@
 let content = document.querySelector('.content')
-let popup = document.querySelector('.popup');
-let profileButtonEdit = content.querySelector('.profile__edit-button');
-let popupClose = popup.querySelector('.popup__close');
-let popupForm = popup.querySelector('.popup__container');
-let nameInput = popupForm.querySelector('.popup__input_type_name');
+let popup = document.querySelector('.popup'); 
+let profileButtonEdit = content.querySelector('.profile__edit-button'); 
+let popupClose = popup.querySelector('.popup__close');  
+let popupForm = popup.querySelector('.popup__container');  
+let nameInput = popupForm.querySelector('.popup__input_type_name'); 
 let jobInput = popupForm.querySelector('.popup__input_type_job');
+let profileName = content.querySelector('.profile__info-name');  
+let profileJob = content.querySelector('.profile__info-job');
 
 function popupChange() {
     popup.classList.toggle('popup_opened'); 
-    popupForm.reset(); // 
   }
   
-  profileButtonEdit.addEventListener('click', popupChange); 
-  popupClose.addEventListener('click', popupChange); 
+
+  profileButtonEdit.addEventListener("click", () => {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
   
+    popupChange();
+  });
+
+  popupClose.addEventListener('click', popupChange);
+
   function handleFormSubmit(evt) {
     evt.preventDefault(); 
     let name = nameInput.value;
     let job = jobInput.value;
-  
-    let profileName = content.querySelector('.profile__info-name');
-    let profileJob = content.querySelector('.profile__info-job');
-  
+
     profileName.textContent = name;
     profileJob.textContent = job;
-  
-    nameInput.setAttribute('value', name);
-    jobInput.setAttribute('value', job);
   
     popupChange();
   }
