b09b77b0405a88862149987788c6a6b1ef852da0
Yuliya Papina <panterajuliapapina@yandex.ru>
2023-02-11 18:21:48 +0300

    сделан попап добавления новых мест

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 let content = document.querySelector('.content')
-let popup = document.querySelector('.popup'); 
+let popup = document.querySelector('.popup_type_edit'); 
 let profileButtonEdit = content.querySelector('.profile__edit-button'); 
 let popupClose = popup.querySelector('.popup__close');  
 let popupForm = popup.querySelector('.popup__form');  
@@ -7,6 +7,9 @@ let nameInput = popupForm.querySelector('.popup__input_type_name');
 let jobInput = popupForm.querySelector('.popup__input_type_job');
 let profileName = content.querySelector('.profile__info-name');  
 let profileJob = content.querySelector('.profile__info-job');
+let openAddButton = content.querySelector('.profile__add-button');
+let popupAdd = document.querySelector('.popup_type_add');
+let popupAddClose = popupAdd.querySelector('.popup__close');
 
 function popupChange() {
     popup.classList.toggle('popup_opened'); 
@@ -20,7 +23,6 @@ function popupChange() {
     popupChange();
   });
 
-  
 
   function handleFormSubmit(evt) {
     evt.preventDefault(); 
@@ -35,4 +37,12 @@ function popupChange() {
   
   popupClose.addEventListener('click', popupChange);
   popupForm.addEventListener('submit', handleFormSubmit);
+
+
+  
+function popupAddChange() {
+  popupAdd.classList.toggle('popup_opened');
+}
+  openAddButton.addEventListener("click", popupAddChange);
+  popupAddClose.addEventListener("click", popupAddChange);
 
