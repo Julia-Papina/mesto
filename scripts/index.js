f468b82c86e9b1c2bd69fd572d4d8c595292e9ae
Yuliya Papina <panterajuliapapina@yandex.ru>
2023-02-13 15:17:49 +0300

    сделан попап-имедж

--- scripts/index.js
+++ scripts/index.js
@@ -39,7 +39,7 @@ function popupChange() {
   popupForm.addEventListener('submit', handleFormSubmit);
 
 
-  
+// открытие попапа добаления фото
 function popupAddChange() {
   popupAdd.classList.toggle('popup_opened');
 }
@@ -51,7 +51,8 @@ function popupAddChange() {
   let initialCards = [
     {
       name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+      alt: 'Архыз'
     },
     {
       name: 'Челябинская область',
@@ -76,14 +77,59 @@ function popupAddChange() {
   ]; 
   initialCards.forEach(function (element) {
     let cadrsTemplate = document.querySelector('#cards').content;
-    let cardsItem = document.querySelector('.elements');
+    let cardsItem = document.querySelector('.cards');
     let cardsElement = cadrsTemplate.querySelector('.cards__item').cloneNode(true);
     cardsElement.querySelector('.cards__image').src = element.link;
+    cardsElement.querySelector('.cards__image').alt = element.name;
+
     cardsElement.querySelector('.cards__title').textContent = element.name;
+
+    cardsElement.querySelector('.cards__like').addEventListener('click', (evt) => {
+      evt.target.classList.toggle('cards__like_active');
+    });
+
+    cardsElement.querySelector('.cards__delete-button').addEventListener('click', (evt) => {
+      evt.target.parentElement.remove();
+    });
+
     cardsItem.append(cardsElement);
   });
 
+
+
+
+
+let popupImage = document.querySelector('.popup_type_image'); //попап
+
+let openPopupImageBtn = content.querySelectorAll('.cards__image'); //все картинки
+
+let popapImageClose = popupImage.querySelector('.popup__close'); //закрытие
+
+let openImage = popupImage.querySelector('.popup__open-image');  //картинка в попапе
+let popapImagePlace = popupImage.querySelector('.popup__place');
  
+function popupImageChange() {
+  popupImage.classList.toggle('popup_opened');  //открытие попапа
+  
+}
+let editPopupImage = (event) => {
+  let link = event.target.getAttribute('src');
+  let place = event.target.getAttribute('alt');
+  openImage.setAttribute('src', link);
+  openImage.setAttribute('alt', place);
+  popapImagePlace.textContent = place;
+
+}
+
+openPopupImageBtn.forEach((element) => element.addEventListener('click', editPopupImage));
+openPopupImageBtn.forEach((element) => element.addEventListener('click', popupImageChange));
+popapImageClose.addEventListener('click', popupImageChange);
+
+
+ 
+
+
+
  
 
 
