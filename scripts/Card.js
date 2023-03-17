e92ae56b76d045945c7c492f360254c42c74219f
Yuliya Papina <panterajuliapapina@yandex.ru>
2023-03-17 11:41:53 +0300

    создала класс Card

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,3 +1,57 @@
-export class Card {
+export default class Card {
+ constructor(data, templateSelector, openPopupFunc) {
+    this._name = data.name;
+    this._link = data.link;
+    this._templateSelector = templateSelector;
+    this._openPopupFunc = openPopupFunc;
+}
+ _getTemplate() {
+ const newCard = document
+    .querySelector(this._templateSelector)
+    .content.querySelector('.cards__item')
+    .cloneNode(true);
+
+ return newCard;
+
+}
+
+ _deleteCard() {
+    this._element.remove();
+    this._element = null;
+}
+
+ _toggleCardLike() {
+    this._likeBtn.classList.toggle('cards__like_active');
+}
+
+ _setEventListeners() {
+    this._image.addEventListener("click", () => {
+      this._openPopupFunc(this._name, this._link);
+    });
+
+    this._deleteBtn.addEventListener("click", () => {
+        this._deleteCard();
+      });
+
+    this._likeBtn.addEventListener("click", () => {
+        this._toggleCardLike();
+      });
+    }
+
+ generateCard() {
+    this._element = this._getTemplate();
+    this._title = this._element.querySelector('.cards__title');
+    this._image = this._element.querySelector('.cards__image');
+    this._likeBtn = this._element.querySelector('.cards__like');
+    this._deleteBtn = this._element.querySelector('.cards__delete-button');
+    
+    this._setEventListeners();
+    
+    this._title.textContent = this._name;
+    this._image.alt = this._name;
+    this._image.src = this._link;
+    
+    return this._element;
+}
 
 }
