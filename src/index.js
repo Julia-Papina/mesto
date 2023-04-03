3d50e7cdbfca701d5df36c588c6bd91147aaf707
Yuliya Papina <panterajuliapapina@yandex.ru>
2023-04-03 10:49:24 +0300

    правка

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
-import * as constants from "../utils/constants.js";
-import Card from "../components/Card.js";
-import FormValidator from "../components/FormValidator.js";
-import Section from "../components/Section.js";
-import PopupWithForm from "../components/PopupWithForm.js";
-import PopupWithImage from "../components/PopupWithImage.js";
-import UserInfo from "../components/UserInfo.js";
+import * as constants from "./utils/constants.js";
+import Card from "./components/Card.js";
+import FormValidator from "./components/FormValidator.js";
+import Section from "./components/Section.js";
+import PopupWithForm from "./components/PopupWithForm.js";
+import PopupWithImage from "./components/PopupWithImage.js";
+import UserInfo from "./components/UserInfo.js";
 
 const userInfo = new UserInfo({
   nameSelector: ".profile__info-name",
